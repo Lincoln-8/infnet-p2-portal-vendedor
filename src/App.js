90d0bb31bed8ed1e2a06f1c86418f3b95cdb1726
Lincoln-8 <Lincoln.Vasconcelos.8@gmail.com>
2022-03-24 22:34:08 -0300

    adicionando bootstrap e react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,24 @@
+import Button from "react-bootstrap/Button";
+import Container from "react-bootstrap/Container";
+import Navbar from "react-bootstrap/Navbar";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+
 function App() {
   return (
-    <p>Meu primeiro REACT limpo!</p>
+    <div className="App">
+      <header>
+        <Navbar bg="light" expand="lg">
+          <Container></Container>
+        </Navbar>
+      </header>
+      <main>
+        <Container>
+          <p>Meu primeiro REACT limpo!</p>
+          <Button>Isso é um botao</Button>
+        </Container>
+      </main>
+    </div>
   );
 }
 
